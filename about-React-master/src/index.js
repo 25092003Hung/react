10697966b3c77e17cdf6b50f9ccd832f0cafcfd4
25092003Hung <hungprodns1234@gmail.com>
2022-11-26 23:13:57 +0700

    first commit

---
+++ about-React-master/src/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Card from "./components/card";
+import Demo from "./components/Demo";
+import HomeLayout from "./components/HomeLayout/HomeLayout";
+import DataBinding from "./databinding/DataBinding";
+import HandleEvent from "./handleEvent/HandleEvent";
+import DemoLogin from "./State/DemoLogin";
+// import css ảnh hưởng toan` bo ung dung
+import "./index.scss";
+import TangGiamFontSize from "./State/TangGiamFontSize/TangGiamFontSize";
+import DemoProps from "./props/DemoProps/DemoProps";
+import BaiTapShop from "./props/BaiTapShop/BaiTapShop";
+import BaiTapXemChiTiet from "./props/BaiTapXemChiTiet/BaiTapXemChiTiet";
+//Cấu hình react router
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Home from "./pages/Home/Home";
+import HomeTemplate from "./templates/HomeTemplate";
+import Page404 from "./pages/Page404/Page404";
+import BaiTapGioHang from "./pages/BaiTapGioHang/BaiTapGioHang";
+import ReactForm from "./pages/Form/ReactForm";
+import ReactLifeCycle from "./pages/ReactLifeCycle/ReactLifeCycle";
+import DemoRedux from "./pages/DemoRedux/DemoRedux";
+
+//Cấu hình redux store
+import { Provider } from "react-redux";
+import { store } from "./redux/configStore";
+import BaiTapGame from "./pages/DemoRedux/BaiTapGame/BaiTapGame";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<HomeTemplate />}>
+            <Route index element={<Home />}></Route>
+            <Route path="home" element={<Home />}></Route>
+            <Route path="register" element={<Register />}></Route>
+            <Route path="login" element={<Login />}></Route>
+            <Route path="btgiohang" element={<BaiTapGioHang />}></Route>
+            <Route path="form" element={<ReactForm />}></Route>
+            <Route path="reactlifecycle" element={<ReactLifeCycle />}></Route>
+            <Route path="demoredux" element={<DemoRedux />}></Route>
+            <Route path="demogame" element={<BaiTapGame />}></Route>
+            {/* <Route path="*" element={<Page404 />}></Route> */}
+            <Route path="*" element={<Navigate to="" />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </>
+);
